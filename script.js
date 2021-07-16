35ce60b8a4daee5ed09e5d8a30fd3e035e7f01a1
sudoer777 <78781902+sudoer777@users.noreply.github.com>
2021-07-16 15:45:44 -0600

    Create refreshBooksView() function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let myLibrary = [];
 
+let booksView = document.getElementById("books-view");
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -12,4 +14,32 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+}
+
+function refreshBooksView() {
+    booksView.innerHTML = "";
+
+    myLibrary.forEach(book => {
+        let bookDisplay = document.createElement('div');
+        bookDisplay.classList.add("book-display");
+        
+        let bookDisplayTitle = document.createElement('span');
+        bookDisplayTitle.classList.add("book-display-title");
+        bookDisplayTitle.textContent = book.title;
+        bookDisplay.appendChild(bookDisplayTitle);
+
+        let bookDisplayAuthor = document.createElement('span');
+        bookDisplayAuthor.classList.add("book-display-author");
+        bookDisplayAuthor.textContent = book.author;
+        bookDisplay.appendChild(bookDisplayAuthor);
+
+        let bookDisplayPages = document.createElement('span');
+        bookDisplayPages.classList.add("book-display-pages");
+        bookDisplayPages.textContent = book.pages;
+        bookDisplay.appendChild(bookDisplayPages);
+
+        if(book.read) bookDisplay.classList.add("book-read");
+
+        booksView.appendChild(bookDisplay);
+    });
 }
